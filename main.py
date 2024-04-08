96f07f926b5891f44c514d2bdf9b2ff564490d59
Kobetc <akobets@gmcs.ru>
2024-04-08 16:51:12 +0300

    Init flask app db.

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+db_name = 'sqlite.db'
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app_dir, db_name)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,  password: str):
+        return check_password_hash(self.password_hash, password)
+    
+class Position(db.Model):
+    __tablename__ = 'positions'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    discription = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.name)
+
 
 @app.route('/')
 def home_page():
